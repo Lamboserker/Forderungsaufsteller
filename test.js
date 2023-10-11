ec00bdf9d79ae0b0b2ce0a880517f5155f28c14d
Lukas Lamberz <johndoe@example.comlukaslamberz96@gmail.com>
2023-10-11 13:20:58 +0200

    changes

--- test.js
+++ test.js
@@ -1,24 +1,25 @@
 // Import required modules
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // Create express app
 const app = express();
-
+console.log("test");
 // Set up middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/mydatabase', { useNewUrlParser: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+mongoose
+  .connect("mongodb://localhost/mydatabase", { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // Define routes
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 // Start server
